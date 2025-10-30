c166c66e853cae3100cc32c9f089b7fe52597fe0
TuanAnhNguyen281 <ntanh2811@gmail.com>
2025-10-30 20:50:49 +0700

    update profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
