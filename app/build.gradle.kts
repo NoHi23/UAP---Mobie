a7c79ca6efae4ea177d8c63f096da498d3569ffe
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-10-31 09:00:52 +0700

    hieunn-commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.13.0")
     implementation(libs.appcompat)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
